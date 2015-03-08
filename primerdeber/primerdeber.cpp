019621a14bb242983a9d601b03dc615a83a2533b
Ana Ruiz <anaparanoia@gmail.com>
2015-03-08 14:32:14 -0500

    Archivos subidos

---
+++ primerdeber/primerdeber.cpp
@@ -0,0 +1,32 @@
+// primerdeber.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include "opencv2/opencv.hpp"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int umbral=100;
+	char filename[33]="C:/Users/ANA/Pictures/bosque.jpg";
+
+	IplImage* img1 = cvLoadImage(filename,1);
+	IplImage* img2 = cvLoadImage(filename,0);
+	IplImage* img3 = cvLoadImage(filename,0);
+	cvThreshold( img2, img3, umbral,255, CV_THRESH_BINARY);
+
+	cvNamedWindow("Imagen a color",0);
+	cvNamedWindow("Imagen escala de grises",0);
+	cvNamedWindow("Imagen binaria",0);
+	cvShowImage("Imagen a color",img1);
+	cvShowImage("Imagen escala de grises",img2);
+	cvShowImage("Imagen binaria",img3);
+
+	cvWaitKey();
+	cvDestroyAllWindows();
+	cvReleaseImage(&img1);
+	cvReleaseImage(&img2);
+	cvReleaseImage(&img3);
+	return 0;
+	
+}
+
